ae0dc450f01e71544479a0ae0cf2cef5e351d4c0
Trantienloc2411 <ttloc2411@gmail.com>
2023-11-21 23:14:52 +0700

    Update Customize - UI

--- BirdCageShop/BirdCageShop/Pages/Users/Customize.cshtml.cs
+++ BirdCageShop/BirdCageShop/Pages/Users/Customize.cshtml.cs
@@ -8,12 +8,31 @@ namespace BirdCageShop.Pages.Users
 {
     public class CustomizeModel : PageModel
     {
-        [BindProperty]
-        public BusinessObjects.Models.Product Product { get; set; }
         private readonly IUploadService _uploadService;
         private readonly IProductRepository _proRepos;
         private readonly ICartRepository _cartRepo;
+
         [BindProperty]
+        public string cageType { get; set; }
+        [BindProperty]
+        public string lidCage {  get; set; }
+        [BindProperty]
+        public string barCageType { get; set; }
+        [BindProperty]
+        public int barCageCount { get; set; } = 50;
+        [BindProperty]
+        public int barCount { get; set; }
+
+        [BindProperty]
+        public string doorCageType { get; set; }
+        [BindProperty]
+        public string doorCageSize { get; set; }
+        [BindProperty]
+        public string baseCage { get;set ; }
+        [BindProperty]
+        public string orderPrice { get; set; }
+        
+        
         public IFormFile CageImg { get; set; }
         public CustomizeModel(IUploadService uploadService)
         {
@@ -22,71 +41,16 @@ namespace BirdCageShop.Pages.Users
             _cartRepo = new CartRepository();
         }
 
-
-        public async Task<IActionResult> OnPostAsync(IFormFile CageImg)
+        public void OnPost()
         {
-            if (HttpContext.Session.GetInt32("userID") == null)
-            {
-                TempData["errorMessage"] = "Hãy đăng nhập trước khi tạo mới lồng của bạn nhé. Mình chuyển đến trang đăng nhập giúp bạn rồi nè";
-                return RedirectToPage("/Login/Index");
-            }
-            TempData["errorMessage"] = "";
-            Product = new BusinessObjects.Models.Product();
-            if (CageImg != null)
-            {
-                Product.CageImg = await _uploadService.UploadFileAsync(CageImg);
-            }
-            string sizeValue = Request.Form["sizeValue"];
-            string quantityValue = Request.Form["quantityValue"];
-            if(quantityValue.Length == 0)
-            {
-                quantityValue = "1";    
-            }
-            string nanValue = Request.Form["nanValue"];
-            string materialValue = Request.Form["materialValue"];
-            string cageNoteValue = Request.Form["cageNote"];
-            string productEst = Request.Form["totalEstimatedPrice"];
-            if (int.TryParse(quantityValue, out int quantity) && int.TryParse(nanValue, out int nan) && decimal.TryParse(productEst,out decimal price))
-            {
-                //Tạo lồng
-                string name = "Lồng tự tạo của " + HttpContext.Session.GetInt32("userID");
-                Product.CageName = name;
-                Product.Quantity = quantity;
-                Product.Material = materialValue;
-                Product.Size = sizeValue;
-                Product.Bar = nan;
-                Product.Description = cageNoteValue;
-                Product.CageStatus = 2;
-                Product.Price = price;
-                int prodID = _proRepos.AddCustomizeCage(Product);
-                if (prodID != 0)
-                {
-                    int addToCart = _cartRepo.addProductToCart(prodID, (int)Product.Quantity, 0);
-                    if (addToCart != 0)
-                    {
-                        TempData["successMessage"] = "Lồng tự tạo của bạn đã nằm trong giỏ hàng!";
-                        return RedirectToPage("./UCart/Index");
-                    }
-                    else
-                    {
-                        TempData["errorMessage"] = "Thêm vào giỏ hàng thất bại!";
-                    }
-
-                }
-                else
-                {
-                    TempData["errorMessage"] = "Tạo lồng thất bại!";
-                }
-                return Page();
-            }
-            else
-            {
-                TempData["errorMessage"] = "Lỗi! Giá trị bạn nhập không hợp lệ (Số nan / Số lượng là số nguyên và lớn hơn 0)";
-                return Page();
-            }
-
-
+            doorCageType = Request.Form["doorCage"];
+            string? orderEst = Request.Form["OrderEst"];
+            string? OrderPrice = Request.Form["OrderPrice"];
+            
+            Page();
         }
+
+
 
     }
 }
